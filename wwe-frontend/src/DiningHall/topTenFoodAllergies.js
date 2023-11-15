9699889ad8a788f2083b57f1a7d183dde172aa06
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2023-11-15 18:11:01 -0500

    Updated dining hall top ten food allergies and rated food frontend

--- wwe-frontend/src/DiningHall/topTenFoodAllergies.js
+++ wwe-frontend/src/DiningHall/topTenFoodAllergies.js
@@ -1,77 +1,48 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
+import Cookies from 'js-cookie';
 
 /* This component should display top ten food allergies common users have. */
 
-// Generate Order Data
-function createData(id, allergy, students, percentage) {
-  return { id, allergy, students, percentage };
-}
-
-const rows = [
-  createData(
-    0,
-    'Milk',
-    3500,
-    22.44,
-  ),
-  createData(
-    1,
-    'Eggs',
-    2998,
-    19.80,
-  ),
-  createData(
-    2, 
-    'Fish',
-    2777,
-    18.70),
-  createData(
-    3,
-    'Crustacean shellfish',
-    1700,
-    15.33,
-  ),
-  createData(
-    4,
-    'Tree nuts',
-    1490,
-    12.89,
-  ),
-];
-
-function preventDefault(event) {
-  event.preventDefault();
-}
-
-export default function TopTenFoodAllergies() {
-  const [userData, setUserData] = useState({top_ten_food_allergies: ''});
-  const [loading, setLoading] = useState(true);
+export default function TopTenFoodAllergies({ userId }) {
+  const [topTenFoodAllergies, setTopTenFoodAllergies] = useState([]);
 
   useEffect(() => {
-      const fetchUserData = async () => {
-          try {
-              const response = await fetch('http://127.0.0.1:8080/get_top_ten_food_allergies');
-              if (!response.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              const data = await response.json();
-              setUserData(data);
-          } catch (error) {
-              console.error('There was a problem with the fetch operation:', error.message);
-          } finally {
-              setLoading(false);
-          }
+    const fetchTopTenFoodAllergies = async () => {
+      const token = Cookies.get('token'); 
+      const apiUrl = `http://127.0.0.1:8080/getTopTenPriorityFoodAllergies`; 
+      console.log(userId);
+      const requestOptions = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': token,
+          },
+          body: JSON.stringify({ 
+            diningHallID: userId
+          })
       };
-      // fetchUserData();
-  }, []);
+
+      try {
+          const response = await fetch(apiUrl, requestOptions);
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          console.log(data);
+          setTopTenFoodAllergies(data.topTenFoodAllergies)
+      } catch (error) {
+          console.error('There was a problem fetching top ten food allergies:', error);
+      }
+    };
+    // fetchTopTenFoodAllergies();
+}, [userId]);
 
   return (
     <React.Fragment>
@@ -85,18 +56,15 @@ export default function TopTenFoodAllergies() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.allergy}</TableCell>
-              <TableCell>{row.students}</TableCell>
-              <TableCell align="right">{`${row.percentage}%`}</TableCell>
+          {topTenFoodAllergies.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.allergy}</TableCell>
+              <TableCell>{item.num_users}</TableCell>
+              <TableCell align="right">{`${item.percentage}%`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more food allergies
-      </Link>
     </React.Fragment>
   );
 }
--- wwe-frontend/src/DiningHall/topTenRatedFood.js
+++ wwe-frontend/src/DiningHall/topTenRatedFood.js
@@ -1,78 +1,48 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
+import Cookies from 'js-cookie';
 
 /* This component should display top ten food items rated by common users. */
 
-// Generate Order Data
-function createData(id, foodItem, rating, rates) {
-  return { id, foodItem, rating, rates };
-}
-
-const rows = [
-  createData(
-    0,
-    'Chicken Curry',
-    4.9,
-    2100,
-  ),
-  createData(
-    1,
-    'Basmati Rice with Peas',
-    4.72,
-    1899,
-  ),
-  createData(
-    2, 
-    'Romaine Kale Lettuce Blend', 
-    4.66,
-    1029,
-  ),
-  createData(
-    3,
-    'Tomato Chutney',
-    4.12,
-    1567,
-  ),
-  createData(
-    4,
-    'Cheeseburger',
-    3.89,
-    2127,
-  ),
-];
-
-function preventDefault(event) {
-  event.preventDefault();
-}
-
-export default function TopTenRatedFood() {
-  const [userData, setUserData] = useState({top_ten_rated_food: ''});
-  const [loading, setLoading] = useState(true);
+export default function TopTenRatedFood({ userId }) {
+  const [topTenRatedFood, setTopTenRatedFood] = useState([]);
 
   useEffect(() => {
-      const fetchUserData = async () => {
-          try {
-              const response = await fetch('http://127.0.0.1:8080/get_top_ten_rated_food');
-              if (!response.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              const data = await response.json();
-              setUserData(data);
-          } catch (error) {
-              console.error('There was a problem with the fetch operation:', error.message);
-          } finally {
-              setLoading(false);
-          }
+    const fetchTopTenRatedFood = async () => {
+      const token = Cookies.get('token'); 
+      const apiUrl = `http://127.0.0.1:8080/getTopTenHighestRatedDishes`; 
+      console.log(userId);
+      const requestOptions = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': token,
+          },
+          body: JSON.stringify({ 
+            diningHallID: userId
+          })
       };
-      // fetchUserData();
-  }, []);
+
+      try {
+          const response = await fetch(apiUrl, requestOptions);
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          console.log(data);
+          setTopTenRatedFood(data.topTenRatedFood)
+      } catch (error) {
+          console.error('There was a problem fetching top ten rated food:', error);
+      }
+    };
+    // fetchTopTenRatedFood();
+}, [userId]);
 
   return (
     <React.Fragment>
@@ -81,23 +51,20 @@ export default function TopTenRatedFood() {
         <TableHead>
           <TableRow>
             <TableCell>Food Item</TableCell>
-            <TableCell>Rating</TableCell>
+            <TableCell>Average Rating</TableCell>
             <TableCell># Rates</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.foodItem}</TableCell>
-              <TableCell>{row.rating}</TableCell>
-              <TableCell>{row.rates}</TableCell>
+          {topTenRatedFood.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>{item.dish_name}</TableCell>
+              <TableCell>{item.average_rating}</TableCell>
+              <TableCell align="right">{item.num_rates}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more food item rates
-      </Link>
     </React.Fragment>
   );
 }
