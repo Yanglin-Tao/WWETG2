839011f7871c10ec24fefbaf4e099d1cda4449f9
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2023-11-25 08:08:18 -0500

    Update display and update common user goals

--- wwe-frontend/src/CommonUser/displayCommonUserGoals.js
+++ wwe-frontend/src/CommonUser/displayCommonUserGoals.js
@@ -20,6 +20,9 @@ import TextField from '@mui/material/TextField';
 import DashboardLayout from './DashboardLayout';
 import Copyright from '../Copyright';
 import dayjs from 'dayjs';
+import Cookies from 'js-cookie';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 /* TODO: This component should display common user's dietary goals
 */
@@ -27,68 +30,103 @@ import dayjs from 'dayjs';
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-function DisplayCommonUserGoals({userId}) {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
-  const login = () => {
-    window.open("/login", "_self");
-  };
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
+function DisplayCommonUserGoals({userId}) {
   const [isEditable, setIsEditable] = useState(false);
   const [startDate, setStartDate] = useState(dayjs('2023-01-01'));
   const [endDate, setEndDate] = useState(dayjs('2023-12-31'));
-  const [calories, setCalories] = useState(2000);
+  const [maxCalories, setMaxCalories] = useState(2000);
+  const [minCalories, setMinCalories] = useState(1000);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertSeverity, setAlertSeverity] = useState('info');
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   const toggleEdit = () => {
     setIsEditable(!isEditable);
+    if (isEditable) {
+      updateCommonUserGoals();
+    }
   };
 
-  const [userData, setUserData] = useState({user_goals: ''});
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-      const fetchUserData = async () => {
-          try {
-              const response = await fetch('http://127.0.0.1:8080/get_common_user_goals');
-              if (!response.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              const data = await response.json();
-              setUserData(data);
-          } catch (error) {
-              console.error('There was a problem with the fetch operation:', error.message);
-          } finally {
-              setLoading(false);
-          }
-      };
-      // fetchUserData();
-  }, []);
+    const fetchCommonUserGoals = async () => {
+        const token = Cookies.get('token'); 
+        const apiUrl = `http://127.0.0.1:8080/getCommonUserGoals`; 
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+            },
+            body: JSON.stringify({ 
+              userID: userId
+            })
+        };
+
+        try {
+            const response = await fetch(apiUrl, requestOptions);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data);
+            setStartDate(data.startDate);
+            setEndDate(data.endDate);
+            setMaxCalories(data.dailyCalorieIntakeMaximum);
+            setMinCalories(data.dailyCalorieIntakeMinimum);
+        } catch (error) {
+            console.error('There was a problem fetching the user goals:', error);
+        }
+    };
+    fetchCommonUserGoals();
+  }, [userId]); 
 
   const updateCommonUserGoals = async () => {
-      try {
-          const response = await fetch('http://127.0.0.1:8080/update_common_user_goals', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                  userId: userData.userId, 
-                  newFoodPreference: userData.user_goals,
-              }),
-          });
+    try {
+        const token = Cookies.get('token'); 
+        const response = await fetch('http://127.0.0.1:8080/updateCommonUserGoals', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+            },
+            body: JSON.stringify({
+              userID: userId,
+              startDate: startDate,
+              endDate: endDate,
+              dailyCalorieIntakeMaximum: maxCalories,
+              dailyCalorieIntakeMinimum: minCalories
+            }),
+        });
 
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        } 
 
-          const responseData = await response.json();
-          // handle the response data 
-
-      } catch (error) {
-          console.error('There was a problem updating common user goals:', error.message);
-      }
+        const responseData = await response.json();
+        console.log('Update successful', responseData);
+        if (responseData.Message === "Successfully updated diet goals info into your profile.") {
+          setAlertSeverity('success');
+          setAlertMessage(responseData.Message);
+          setOpen(true);
+        } else {
+          setAlertSeverity('error');
+          setAlertMessage(responseData.Message);
+          setOpen(true);
+        }
+    } catch (error) {
+        console.error('There was a problem updating common user goals:', error.message);
+    }
   };
 
   return (
@@ -109,13 +147,22 @@ function DisplayCommonUserGoals({userId}) {
           }}
         >
           <Toolbar />
+          <Snackbar
+              open={open}
+              autoHideDuration={6000}
+              onClose={handleClose}
+            >
+              <Alert onClose={handleClose} severity={alertSeverity} sx={{ width: '100%' }}>
+                {alertMessage}
+              </Alert>
+          </Snackbar>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
               <Grid container spacing={3}>
                 <Grid item xs={12}>
                   <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                     <Title>Current Goals</Title>
-                    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <Box sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }}>
                       <Box sx={{ my: 3, mx: 2 }}>
                         <Grid container alignItems="center">
                           <Grid item xs>
@@ -127,17 +174,31 @@ function DisplayCommonUserGoals({userId}) {
                             {isEditable ? (
                               <TextField
                                 type="number"
-                                value={calories}
-                                onChange={(e) => setCalories(e.target.value)}
+                                label="Max cal per day"
+                                value={maxCalories}
+                                onChange={(e) => setMaxCalories(e.target.value)}
                               />
                             ) : (
                               <Typography gutterBottom variant="h6" component="div">
-                                {calories} cal per day
+                                {maxCalories} max per day
+                              </Typography>
+                            )}
+                            {isEditable ? (
+                              <TextField
+                                type="number"
+                                label="Min cal per day"
+                                value={minCalories}
+                                onChange={(e) => setMinCalories(e.target.value)}
+                                style={{ marginLeft: '10px' }}
+                              />
+                            ) : (
+                              <Typography gutterBottom variant="h6" component="div">
+                                {minCalories} min per day
                               </Typography>
                             )}
                           </Grid>
                         </Grid>
-                        <Typography color="text.secondary" variant="body2">
+                        <Typography color="text.secondary" variant="body2" style={{ marginTop: '10px' }}>
                           The total number of calories a person needs each day varies depending on a number of factors,
                           including the person's age, sex, height, weight, and level of physical activity.
                         </Typography>
