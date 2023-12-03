0457533867576bc8e993e5d8e546994e9cb6706b
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2023-12-02 23:33:24 -0500

    Finished display/update common user privacy settings

--- wwe-frontend/src/CommonUser/displayCommonUserPrivacySettings.js
+++ wwe-frontend/src/CommonUser/displayCommonUserPrivacySettings.js
@@ -4,74 +4,115 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
 import Title from './Title';
+import Typography from '@mui/material/Typography';
 import DashboardLayout from './DashboardLayout';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Copyright from '../Copyright';
+import Cookies from 'js-cookie';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
 
 /* TODO: This component should display common user's privacy settings
 */
 
-function DisplayCommonUserPrivacySettings({userId}) {
-    const [open, setOpen] = React.useState(true);
-    const toggleDrawer = () => {
-        setOpen(!open);
-    };
-    const login = () => {
-        window.open("/login", "_self");
-    };
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
-    const [userData, setUserData] = useState({foodAllgeryPrivacy: true, foodPreferencePrivacy: true, otherPrivacy: true});
-    const [loading, setLoading] = useState(true);
+function DisplayCommonUserPrivacySettings({userId}) {
+    const [isEditable, setIsEditable] = useState(false);
+    const [privacySetting, setPrivacySetting] = useState(true);
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertSeverity, setAlertSeverity] = useState('info');
+    const [open, setOpen] = useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
 
     useEffect(() => {
-        const fetchUserData = async () => {
+        const fetchPrivacySetting = async () => {
+            const token = Cookies.get('token'); 
+            const apiUrl = `http://127.0.0.1:8080/getPrivacySetting`; 
+            console.log(userId);
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token,
+                },
+                body: JSON.stringify({ 
+                    userID: userId
+                })
+            };
+    
             try {
-                const response = await fetch('http://127.0.0.1:8080/get_user_privacy_settings');
+                const response = await fetch(apiUrl, requestOptions);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                setUserData(data);
+                console.log(data);
+                setPrivacySetting(data.allowToCollectData)
             } catch (error) {
-                console.error('There was a problem with the fetch operation:', error.message);
-            } finally {
-                setLoading(false);
+                console.error('There was a problem fetching privacy setting', error);
             }
         };
-        // fetchUserData();
-    }, []);
+        fetchPrivacySetting();
+    }, [userId]);
 
-    const updateCommonUserPrivacySettings = async () => {
+    const updatePrivacySetting = async () => {
+        console.log("privacy setting in updatePrivacySetting");
+        console.log(privacySetting);
+        const token = Cookies.get('token'); 
+        const apiUrl = `http://127.0.0.1:8080/setPrivacySetting`; 
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+            },
+            body: JSON.stringify({ 
+                userID: userId,
+                allowToCollectData: privacySetting
+            })
+        };
+
         try {
-            const response = await fetch('http://127.0.0.1:8080/update_user_privacy_settings', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    foodAllgeryPrivacy: userData.foodAllgeryPrivacy, 
-                    foodPreferencePrivacy: userData.foodPreferencePrivacy, 
-                    otherPrivacy: userData.otherPrivacy, 
-                }),
-            });
-
+            const response = await fetch(apiUrl, requestOptions);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-
-            const responseData = await response.json();
-            // handle the response data 
-
+            const data = await response.json();
+            console.log(data);
+            if (data.message === "Privacy setting updated seccessfully.") {
+                setAlertSeverity('success');
+                setAlertMessage(data.message);
+                setOpen(true);
+            } else {
+                setAlertSeverity('error');
+                setAlertMessage(data.message);
+                setOpen(true);
+            }
         } catch (error) {
-            console.error('There was a problem updating the privacy settings:', error.message);
+            console.error('There was a problem updating privacy setting', error);
+        }
+    };
+
+    const toggleEdit = () => {
+        setIsEditable(!isEditable);
+        if (isEditable) {
+            updatePrivacySetting();
         }
     };
 
@@ -93,17 +134,47 @@ function DisplayCommonUserPrivacySettings({userId}) {
             }}
             >
             <Toolbar />
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                >
+                <Alert onClose={handleClose} severity={alertSeverity} sx={{ width: '100%' }}>
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
                 <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
                     <Title>Privacy Settings</Title>
-                    Authorize dining halls to collect the following data:
                     <FormGroup>
-                    <FormControlLabel control={<Switch defaultChecked />} label="Food allergies data" />
-                    <FormControlLabel control={<Switch defaultChecked />} label="Food preferences data" />
-                    <FormControlLabel control={<Switch defaultChecked />} label="Other account data" />
+                    {isEditable ? (
+                        <FormControlLabel control={<Switch checked={privacySetting} onChange={() => setPrivacySetting(!privacySetting)} />} label="Authorize dining halls to collect my account data" />
+                    ) : (
+                        <FormControlLabel control={<Switch checked={privacySetting} disabled />} label="Authorize dining halls to collect my account data" />
+                    )
+                    }
                     </FormGroup>
+                    { privacySetting ? (
+                        <Typography color="text.secondary" variant="body2">
+                            By authorizing dining halls to collect my account data, I agree that analytics report will be generated 
+                            and displayed to administrative users based on information including but not limited to food allergies and preferences.
+                        </Typography>)
+                    : (
+                        <Typography color="text.secondary" variant="body2">
+                            Your account data will not be collected and used for generating dining hall analytics.
+                        </Typography>)
+                    }
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        onClick={toggleEdit}
+                      >
+                        {isEditable ? 'Save Privacy Settings' : 'Update Privacy Settings'}
+                    </Button>
                     </Paper>
                 </Grid>
                 </Grid>
